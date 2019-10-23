30d6a01c9e82ad58943b5e31bab113fb8f09f5be
Micah Smith <micahjsmith@gmail.com>
2019-10-23 00:24:44 -0400

    Remove SO identifier

--- python/src/util.py
+++ python/src/util.py
@@ -1,7 +1,7 @@
 def is_binary_string(bytes):
     '''Check if bytestring contains binary data
 
-    Source: J.F. Sebastian (https://stackoverflow.com/a/7392391/2514228)
+    Source: J.F. Sebastian (https://stackoverflow.com/a/7392391)
     '''
     textchars = bytearray({7,8,9,10,12,13,27} | set(range(0x20, 0x100)) - {0x7f})
     return bool(bytes.translate(None, textchars))
@@ -9,7 +9,7 @@ def is_binary_string(bytes):
 def is_binary_file(path):
     '''Check if file contains binary data
 
-    Source: J.F. Sebastian (https://stackoverflow.com/a/7392391/2514228)
+    Source: J.F. Sebastian (https://stackoverflow.com/a/7392391)
     '''
     with open(path, 'rb') as f:
         return is_binary_string(f.read(1024))
